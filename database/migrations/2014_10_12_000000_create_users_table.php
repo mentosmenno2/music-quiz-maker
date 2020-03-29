5449e7b11675b45156ead596bad61ccc44184b09
mentosmenno2 <mentosmenno2@hotmail.com>
2020-03-29 18:55:59 +0200

    remove unsused table cols

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,6 @@ class CreateUsersTable extends Migration
 			$table->string('email')->unique();
 			$table->timestamp('email_verified_at')->nullable();
 			$table->string('password');
-			$table->string('access_token')->unique()->nullable()->default(null);
-			$table->bigInteger('access_token_expire')->nullable()->default(null);
 			$table->rememberToken();
 			$table->timestamps();
 		});
